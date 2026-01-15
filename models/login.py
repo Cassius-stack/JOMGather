3e36c419daac04f88445c0e5000e51e528048dff
Popguy198 <deongoh119@gmail.com>
2026-01-15 15:37:54 +0800

    Added admin emails

--- models/login.py
+++ models/login.py
@@ -1,1 +1,8 @@
-print("Hello World 2!")
+from flask import Flask, render_template, request, jsonify
+
+login = Flask(__name__)
+
+users = []
+ADMIN_EMAIL = ["Aadmin@system.com","Badmin@system.com","Cadmin@system.com","Dadmin@system.com","Zadmin@system.com"]
+
+@login.route('/')
