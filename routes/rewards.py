daefa3e493a4232815b9cd3f652924aa554086bd
akilbh <your-email@example.com>
2026-01-21 19:13:32 +0800

    Changed rewards items to be Dynamically added, made button turn grey when insufficient coins

--- routes/rewards.py
+++ routes/rewards.py
@@ -12,4 +12,15 @@ def redeem():
     """View redeem page."""
     user_id = 1  # TODO: Get from session after login
     user_coins = get_user_coins(user_id)
-    return render_template('rewards/redeem.html', coins=user_coins)
+    
+    # List of rewards (can be moved to database later)
+    rewards = [
+        {'id': 1, 'name': 'Welcome Gift', 'price': 200, 'image': 'giftbox.svg'},
+        {'id': 2, 'name': 'Tote Bag', 'price': 500, 'image': 'bulb.svg'},
+        {'id': 3, 'name': 'Bottled Water (1L)', 'price': 1000, 'image': 'giftbox.svg'},
+        {'id': 4, 'name': 'Wallowing Willow', 'price': 1500, 'image': 'bulb.svg'},
+        {'id': 5, 'name': 'Mystery Item (1 of 3)', 'price': 2000, 'image': 'giftbox.svg'},
+        {'id': 6, 'name': 'iPhone 8 Plus', 'price': 3000, 'image': 'bulb.svg'},
+    ]
+    
+    return render_template('rewards/redeem.html', coins=user_coins, rewards=rewards)
