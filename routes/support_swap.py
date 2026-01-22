329e54b83c6b1456660f9fac5f8b7c4f7f19e31c
kevinyzr <yang2007777@gmail.com>
2026-01-22 13:39:20 +0800

    update error

--- routes/support_swap.py
+++ routes/support_swap.py
@@ -7,40 +7,41 @@ from flask import Blueprint, render_template, request, redirect, url_for
 
 support_swap_bp = Blueprint('support_swap', __name__)
 
-@support_swap_bp.route('/library')
-def support_library():
+@support_swap_bp.route('/ss_profile')
+def ss_profile():
     """Browse available support swaps."""
     # TODO: Fetch support swaps from database
-    return render_template('support_swap/support_library.html')
+    return render_template('support_swap/ss_profile.html')
 
-@support_swap_bp.route('/assignment', methods=['GET', 'POST'])
-def support_assignment():
-    """Assign support skills to your profile."""
+@support_swap_bp.route('/ss_activity')
+def ss_activity():
+    """Browse available support swaps."""
+    # TODO: Fetch support swaps from database
+    return render_template('support_swap/ss_activity.html')
+
+@support_swap_bp.route('/ss_match', methods=['GET', 'POST'])
+def ss_match():
+    """View support matches and handle match requests."""
     if request.method == 'POST':
         # TODO: Update user support skills in database
         pass
-    return render_template('support_swap/support_assignment.html')
-
-@support_swap_bp.route('/match')
-def support_match():
-    """View support matches."""
     # TODO: Find matching users based on support needs
-    return render_template('support_swap/support_match.html')
+    return render_template('support_swap/ss_match.html')
 
 @support_swap_bp.route('/request/<int:user_id>', methods=['POST'])
 def request_support_match(user_id):
     """Request a support swap with a user."""
     # TODO: Create support match request
-    return redirect(url_for('support_swap.support_match'))
+    return redirect(url_for('support_swap.ss_match'))
 
 @support_swap_bp.route('/accept/<int:match_id>', methods=['POST'])
 def accept_support_match(match_id):
     """Accept a support swap request."""
     # TODO: Update match status
-    return redirect(url_for('support_swap.support_match'))
+    return redirect(url_for('support_swap.ss_match'))
 
 @support_swap_bp.route('/complete/<int:match_id>', methods=['POST'])
 def complete_support_session(match_id):
     """Mark a support session as complete and earn rewards."""
     # TODO: Award points and badges
-    return redirect(url_for('support_swap.support_match'))
+    return redirect(url_for('support_swap.ss_match'))
