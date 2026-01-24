c58c00f9c3084a30de22081bf1db3d5c116ebace
Popguy198 <deongoh119@gmail.com>
2026-01-24 11:39:54 +0800

    Deleted login

--- models/login.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template, request, jsonify
-
-login = Flask(__name__)
-
-users = []
-ADMIN_EMAIL = ["Aadmin@system.com","Badmin@system.com","Cadmin@system.com","Dadmin@system.com","Zadmin@system.com"]
-
-@login.route('/')
